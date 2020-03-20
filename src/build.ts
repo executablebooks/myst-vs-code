efd03599b881cf61cd164da0a5bc02c9d32bf97c
Chris Sewell <chrisj_sewell@hotmail.com>
2020-03-20 01:57:17 +0000

    convert build to typescript

--- src/build.ts
+++ src/build.ts
@@ -7,9 +7,9 @@ import * as plist from 'plist'
 import * as jinja from 'nunjucks'
 
 const buildGrammar = () => {
-    const templateYaml = fs.readFileSync(path.join(__dirname, 'template/myst.tmLanguage.j2.yaml'), 'utf8')
-    const languageYaml = fs.readFileSync(path.join(__dirname, 'template/languages.yaml'), 'utf8')
-    const directiveYaml = fs.readFileSync(path.join(__dirname, 'template/directives.yaml'), 'utf8')
+    const templateYaml = fs.readFileSync(path.join(__dirname, '../template/myst.tmLanguage.j2.yaml'), 'utf8')
+    const languageYaml = fs.readFileSync(path.join(__dirname, '../template/languages.yaml'), 'utf8')
+    const directiveYaml = fs.readFileSync(path.join(__dirname, '../template/directives.yaml'), 'utf8')
 
     // read variables
     const languages = yaml.safeLoad(languageYaml)
@@ -22,7 +22,7 @@ const buildGrammar = () => {
     // dump to plist
     const grammar = yaml.safeLoad(inputYaml)
     const plistString = plist.build(grammar)
-    fs.writeFileSync(path.join(__dirname, 'syntaxes', 'myst.tmLanguage'), plistString)
+    fs.writeFileSync(path.join(__dirname, '../syntaxes', 'myst.tmLanguage'), plistString)
 }
 
 buildGrammar()
