8dcfacce5755812b8915d723b96f2b2f1f373091
Chris Sewell <chrisj_sewell@hotmail.com>
2020-03-20 00:47:34 +0000

    try using src

--- src/extension.js
+++ src/extension.js
@@ -5,7 +5,8 @@ import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+// export function activate(_context: vscode.ExtensionContext) {
+export function activate(_context) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
