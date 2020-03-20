08d8fd96f6427864fa2264109dc437c97df2b1b1
Chris Sewell <chrisj_sewell@hotmail.com>
2020-03-20 01:00:43 +0000

    run compile in tests

--- src/extension.ts
+++ src/extension.ts
@@ -5,8 +5,7 @@ import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-// export function activate(_context: vscode.ExtensionContext) {
-export function activate(_context) {
+export function activate(_context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
