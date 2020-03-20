e43d96f25adee606474331549ea1a2cee771f172
Chris Sewell <chrisj_sewell@hotmail.com>
2020-03-20 00:34:48 +0000

    test

--- src/extension.ts
+++ src/extension.ts
@@ -9,19 +9,19 @@ export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "myst-highlight" is now active!');
+    console.log('The extension "myst-highlight" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+    // // The command has been defined in the package.json file
+    // // Now provide the implementation of the command with  registerCommand
+    // // The commandId parameter must match the command field in package.json
+    // let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    //     // The code you place here will be executed every time your command is executed
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+    //     // Display a message box to the user
+    //     vscode.window.showInformationMessage('Hello World!');
+    // });
 
-    context.subscriptions.push(disposable);
+    // context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
