43b468fc00987d911cb4815aa2dd8c36c7ef193b
Chris Sewell <chrisj_sewell@hotmail.com>
2021-07-06 16:34:36 +0200

    🔧 MAINTAIN: Update linting

--- src/directives.ts
+++ src/directives.ts
@@ -1,21 +1,22 @@
-'use strict'
-import * as vscode from 'vscode'
-import * as yaml from 'js-yaml'
-import * as dirDict from './data/directives.json'
+"use strict"
+import * as vscode from "vscode"
+import * as yaml from "js-yaml"
+import * as dirDict from "./data/directives.json"
 
-const dirRegexp = new RegExp('.*[`]{3,}\\{')  // TODO check not backslash escapes
-const dirStartRegexp = new RegExp('\\{[a-zA-Z0-9\\-]+\\}')
-
+const dirRegexp = new RegExp(".*[`]{3,}\\{") // TODO check not backslash escapes
+const dirStartRegexp = new RegExp("\\{[a-zA-Z0-9\\-]+\\}")
 
 export function getDirectiveData(name: string) {
-  const dict: {[key: string]: any} = dirDict
+  const dict: { [key: string]: any } = dirDict
   return dict[name]
 }
 
 export class CompletionItemProvider implements vscode.CompletionItemProvider {
-
-  public provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, _token: vscode.CancellationToken) {
-
+  public provideCompletionItems(
+    document: vscode.TextDocument,
+    position: vscode.Position,
+    _token: vscode.CancellationToken
+  ) {
     const { line, character } = position
     if (character < 3) {
       return []
@@ -24,7 +25,7 @@ export class CompletionItemProvider implements vscode.CompletionItemProvider {
     const startLine = textLine.text.slice(undefined, character)
     if (dirRegexp.test(startLine)) {
       const completions: vscode.CompletionItem[] = []
-      const dict: {[key: string]: any} = dirDict
+      const dict: { [key: string]: any } = dirDict
       for (const name in dirDict) {
         directiveCompletion(name, dict[name], completions)
       }
@@ -34,54 +35,59 @@ export class CompletionItemProvider implements vscode.CompletionItemProvider {
   }
 }
 
-
 export function makeDescription(data: any, addClass: boolean = false) {
   const opts = yaml.safeDump({
-    'Required Args': data['required_arguments'],
-    'Optional Args': data['optional_arguments'],
-    'Has Content': data['has_content'],
-    'Options': data['options']
+    "Required Args": data["required_arguments"],
+    "Optional Args": data["optional_arguments"],
+    "Has Content": data["has_content"],
+    Options: data["options"]
   })
-  let string = ''
+  let string = ""
   if (addClass) {
-    string = '`' + data['klass'] + '`\n\n'
+    string = "`" + data["klass"] + "`\n\n"
   }
-  string = string + data['description']
+  string = string + data["description"]
   const mdown = new vscode.MarkdownString(string)
-  mdown.appendCodeblock(opts, 'yaml')
+  mdown.appendCodeblock(opts, "yaml")
   return mdown
 }
 
-
-export function directiveCompletion(name: string, data: any, completions: vscode.CompletionItem[]) {
+export function directiveCompletion(
+  name: string,
+  data: any,
+  completions: vscode.CompletionItem[]
+) {
   const completion = new vscode.CompletionItem(name, vscode.CompletionItemKind.Class)
-  completion.detail = data['klass']
+  completion.detail = data["klass"]
   completion.documentation = makeDescription(data)
   // completion.insertText = new vscode.SnippetString()
   completions.push(completion)
 }
 
 export class HoverProvider implements vscode.HoverProvider {
-
-  public provideHover(document: vscode.TextDocument, position: vscode.Position, _token: vscode.CancellationToken): Promise<vscode.Hover> {
+  public provideHover(
+    document: vscode.TextDocument,
+    position: vscode.Position,
+    _token: vscode.CancellationToken
+  ): Promise<vscode.Hover> {
     const range = document.getWordRangeAtPosition(position, dirStartRegexp)
     if (range === undefined) {
-      return new Promise((resolve) => resolve())
+      return new Promise(resolve => resolve())
     }
     const textLine = document.lineAt(range.start)
     const startLine = textLine.text.slice(undefined, range.start.character)
-    if (!(startLine.endsWith('```'))) {
-      return new Promise((resolve) => resolve())
+    if (!startLine.endsWith("```")) {
+      return new Promise(resolve => resolve())
     }
     // TODO check for backslash escapes
     let text = document.getText(range)
     text = text.slice(1, -1)
     if (!(text in dirDict)) {
-      return new Promise((resolve) => resolve())
+      return new Promise(resolve => resolve())
     }
     const data = getDirectiveData(text)
     const mkdown = makeDescription(data, true)
     const hover = new vscode.Hover(mkdown, range)
-    return new Promise((resolve) => resolve(hover))
+    return new Promise(resolve => resolve(hover))
   }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,49 +1,50 @@
-'use strict'
+"use strict"
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode'
-import { CompletionItemProvider, HoverProvider } from './directives'
-import { markitPlugin } from './md_it_plugin'
+import * as vscode from "vscode"
+import { CompletionItemProvider, HoverProvider } from "./directives"
+import { markitPlugin } from "./md_it_plugin"
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log("Activated MyST-Markdown extension")
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Activated MyST-Markdown extension')
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand("myst.Activate", () => {
+    // The code you place here will be executed every time your command is executed
+    // Display a message box to the user
+    vscode.window.showInformationMessage("Activated MyST-Markdown!")
+  })
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    const disposable = vscode.commands.registerCommand('myst.Activate', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Activated MyST-Markdown!')
-    })
+  context.subscriptions.push(disposable)
 
-    context.subscriptions.push(disposable)
+  const mdSelector: vscode.DocumentSelector = { scheme: "file", language: "markdown" }
 
-    const mdSelector: vscode.DocumentSelector = { scheme: 'file', language: 'markdown' }
+  context.subscriptions.push(
+    vscode.languages.registerCompletionItemProvider(
+      mdSelector,
+      new CompletionItemProvider(),
+      "{"
+    )
+  )
+  context.subscriptions.push(
+    vscode.languages.registerHoverProvider(mdSelector, new HoverProvider())
+  )
 
-    context.subscriptions.push(
-        vscode.languages.registerCompletionItemProvider(
-            mdSelector, new CompletionItemProvider(), '{'))
-    context.subscriptions.push(
-        vscode.languages.registerHoverProvider(
-            mdSelector, new HoverProvider()))
-
-    if (!vscode.workspace.getConfiguration('myst.preview').get('enable', true)){
-        return {}
+  if (!vscode.workspace.getConfiguration("myst.preview").get("enable", true)) {
+    return {}
+  }
+  return {
+    extendMarkdownIt(md: any) {
+      return md.use(markitPlugin)
     }
-    return {
-        extendMarkdownIt(md: any) {
-            return md.use(markitPlugin)
-        }
-    }
-
+  }
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
