002a679d3b7bedf6d4fde7c6df6f95a8cdd5c8d1
Chris Sewell <chrisj_sewell@hotmail.com>
2021-07-06 15:14:35 +0200

    Update runIntergration.ts

--- src/test/runIntergration.ts
+++ src/test/runIntergration.ts
@@ -13,7 +13,7 @@ async function main() {
         const extensionTestsPath = path.resolve(__dirname, './integration/index')
 
         // Download VS Code, unzip it and run the integration test
-        await runTests({ extensionDevelopmentPath, extensionTestsPath, version: '1.56.2' })
+        await runTests({ extensionDevelopmentPath, extensionTestsPath, version: '1.55.2' })
     } catch (err) {
         console.error(err)
         console.error('Failed to run tests')
